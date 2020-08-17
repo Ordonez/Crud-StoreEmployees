826161075b586d31101b38cd3dbad7f8378704a6
Eric Ordonez <jwustudent@gmail.com>
2020-08-17 02:56:55 -0500

    Creating project structure

--- src/main/java/com/SpringbootTraining/CrudStoreEmployees/CrudStoreEmployeesApplication.java
+++ src/main/java/com/SpringbootTraining/CrudStoreEmployees/CrudStoreEmployeesApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CrudStoreEmployeesApplication {
-
-	//Testing Testing 
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CrudStoreEmployeesApplication.class, args);
 	}
