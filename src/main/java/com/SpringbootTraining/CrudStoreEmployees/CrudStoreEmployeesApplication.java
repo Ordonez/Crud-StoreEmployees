63b52ae78f7a6d8386152da0827eff3e8d303e08
Eric Ordonez <jwustudent@gmail.com>
2020-08-17 02:33:19 -0500

    Creating spring boot crud project structure

---
+++ src/main/java/com/SpringbootTraining/CrudStoreEmployees/CrudStoreEmployeesApplication.java
@@ -0,0 +1,13 @@
+package com.SpringbootTraining.CrudStoreEmployees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudStoreEmployeesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudStoreEmployeesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SpringbootTraining/CrudStoreEmployees/CrudStoreEmployeesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringbootTraining.CrudStoreEmployees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudStoreEmployeesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
