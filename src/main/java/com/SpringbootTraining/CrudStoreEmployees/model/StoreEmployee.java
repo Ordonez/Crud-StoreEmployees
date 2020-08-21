501b1f484fcb2625544b58d73022e6ab595662c1
Eric Ordonez <jwustudent@gmail.com>
2020-08-21 04:24:28 -0500

    Coded StoreEmployee entity file

---
+++ src/main/java/com/SpringbootTraining/CrudStoreEmployees/model/StoreEmployee.java
@@ -0,0 +1,71 @@
+package com.SpringbootTraining.CrudStoreEmployees.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "student_users") //table name within StoreEmployees DB
+
+public class StoreEmployee {
+
+    @Id
+    @GeneratedValue
+
+    private int id;
+    private String fname;
+    private String lname;
+    private String email;
+    private int birthyear;
+
+    //setting getters and setters
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getBirthyear() {
+        return birthyear;
+    }
+
+    public void setBirthyear(int birthyear) {
+        this.birthyear = birthyear;
+    }
+}
